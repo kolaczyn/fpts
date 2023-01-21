f889ba4c700b7f635c99ce7ece17e2b12fe86e33
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-01-21 16:12:15 +0100

    add fizzbuzz example

---
+++ src/examples/fizz-buzz.ts
@@ -0,0 +1,18 @@
+import { pipe } from '../pipe/pipe'
+import { range } from '../range/range'
+
+export const fizzBuzz = () =>
+  range(100)
+    .map(num =>
+      pipe(
+        num,
+        num => num + 1,
+        num => {
+          if (num % 15 === 0) return 'Fizzbuzz'
+          if (num % 3 === 0) return 'Fizz'
+          if (num % 5 === 0) return 'Buzz'
+          return num.toString()
+        }
+      )
+    )
+    .join('\n')
