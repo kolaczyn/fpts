31434707bd13053cf868bfb1825f3c8e96806369
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-01-21 15:15:54 +0100

    better types for pipe operator

--- src/pipe/pipe.spec.ts
+++ src/pipe/pipe.spec.ts
@@ -1,9 +1,19 @@
 import { pipe } from './pipe'
 
-test('pipe', () => {
-  const addOne = (n: number) => n + 1
-  const square = (n: number) => n ** 2
-  const decorate = (n: number) => `> ${n} <`
+const addOne = (n: number) => n + 1
+const square = (n: number) => n ** 2
+const decorate = (n: number) => `> ${n} <`
 
-  expect(pipe(1, addOne, square, decorate)).toBe('> 4 <')
+describe('pipe', () => {
+  test('1 function', () => {
+    expect(pipe(3, addOne)).toBe(4)
+  })
+
+  test('2 functions', () => {
+    expect(pipe(3, addOne, square)).toBe(16)
+  })
+
+  test('3 functions', () => {
+    expect(pipe(3, addOne, square, decorate)).toBe('> 16 <')
+  })
 })
--- src/pipe/pipe.ts
+++ src/pipe/pipe.ts
@@ -1,1 +1,19 @@
-export const pipe = (arg0: any, ...functions: any[]) => functions.reduce((accumulator, f) => f(accumulator), arg0)
+type Pipe = {
+  <A, B, C, D, E, F, G, H, Z>(
+    a: A,
+    f0: (a: A) => B,
+    f1?: (b: B) => C,
+    f2?: (c: C) => D,
+    f3?: (d: D) => E,
+    f4?: (e: E) => F,
+    f5?: (e: F) => G,
+    f6?: (e: G) => H,
+    f7?: (f: H) => Z
+  ): Z
+}
+
+export const pipe: Pipe = (arg0, ...functions) =>
+  // we know better than TypeScript in this case, hence ignores
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  functions.reduce((acc, func) => func(acc), arg0)
