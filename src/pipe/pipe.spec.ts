9d0ab5e4a8ddd91954994a4b92ce8099a2933ad8
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-01-21 15:22:18 +0100

    add testing TS and improve pipe code

--- src/pipe/pipe.spec.ts
+++ src/pipe/pipe.spec.ts
@@ -3,6 +3,7 @@ import { pipe } from './pipe'
 const addOne = (n: number) => n + 1
 const square = (n: number) => n ** 2
 const decorate = (n: number) => `> ${n} <`
+const removeSpace = (str: string) => str.replaceAll(' ', '')
 
 describe('pipe', () => {
   test('1 function', () => {
@@ -15,5 +16,20 @@ describe('pipe', () => {
 
   test('3 functions', () => {
     expect(pipe(3, addOne, square, decorate)).toBe('> 16 <')
+  })
+
+  test('4 functions', () => {
+    expect(pipe(3, addOne, square, decorate, removeSpace)).toBe('>16<')
+  })
+
+  test('TypeScript error', () => {
+    expect(
+      pipe(
+        1,
+        n => n + 1,
+        // @ts-expect-error: detect that `number` doesn't have `length` property
+        n => n.length
+      )
+    ).toBe(undefined)
   })
 })
--- src/pipe/pipe.ts
+++ src/pipe/pipe.ts
@@ -1,19 +1,15 @@
-type Pipe = {
-  <A, B, C, D, E, F, G, H, Z>(
-    a: A,
-    f0: (a: A) => B,
-    f1?: (b: B) => C,
-    f2?: (c: C) => D,
-    f3?: (d: D) => E,
-    f4?: (e: E) => F,
-    f5?: (e: F) => G,
-    f6?: (e: G) => H,
-    f7?: (f: H) => Z
-  ): Z
-}
+type Pipe = <A, B, C, D, E, F, G, H, Z>(
+  a: A,
+  f0: (a: A) => B,
+  f1?: (b: B) => C,
+  f2?: (c: C) => D,
+  f3?: (d: D) => E,
+  f4?: (e: E) => F,
+  f5?: (e: F) => G,
+  f6?: (e: G) => H,
+  f7?: (f: H) => Z
+) => Z
 
 export const pipe: Pipe = (arg0, ...functions) =>
-  // we know better than TypeScript in this case, hence ignores
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
+  // @ts-expect-error: we know better than TypeScript
   functions.reduce((acc, func) => func(acc), arg0)
