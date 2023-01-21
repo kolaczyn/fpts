2d6f5fd6602ee099de276a01de0db14907d1c50d
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-01-21 23:55:57 +0100

    add lookup

---
+++ src/arrayAt/lookup/lookup.spec.ts
@@ -0,0 +1,19 @@
+import { none } from '../../primitives/none'
+import { some } from '../../primitives/some'
+import { lookup, Lookup } from './lookup'
+
+test('lookup', () => {
+  const exampleLookup0 = lookup<string>({})
+  expect(exampleLookup0.getAll()).toMatchObject({})
+
+  const exampleLookup1 = exampleLookup0.set('hello', 'world')
+  expect(exampleLookup1.getAll()).toMatchObject({
+    hello: 'world',
+  })
+
+  expect(exampleLookup1.getOr('foo', 'bar')).toEqual('bar')
+
+  expect(exampleLookup1.get('hello')).toEqual(some('world'))
+  const exampleLookup2 = exampleLookup1.remove('hello')
+  expect(exampleLookup2.get('hello')).toEqual(none)
+})
---
+++ src/arrayAt/lookup/lookup.ts
@@ -0,0 +1,39 @@
+import { Maybe, unwrapOr } from '../../maybe/maybe'
+import { pipe } from '../../pipe/pipe'
+import { none } from '../../primitives/none'
+import { some } from '../../primitives/some'
+
+export type Lookup<T> = {
+  set: (key: string, value: T) => Lookup<T>
+  get: (key: string) => Maybe<T>
+  getOr: (key: string, fallback: T) => T
+  remove: (key: string) => Lookup<T>
+  getAll: () => Record<string, T>
+  _data: Record<string, T>
+}
+
+const removeEntryFromRecord = <T>(key: string, record: Record<string, T>) =>
+  Object.entries(record)
+    .filter(([currKey]) => currKey !== key)
+    .reduce<Record<string, T>>(
+      (acc, [key, value]) => ({
+        ...acc,
+        [key]: value,
+      }),
+      {}
+    )
+
+export const lookup = <T>(_data: Record<string, T>): Lookup<T> => ({
+  _data,
+  get: (key: string) => (key in _data ? some(_data[key]) : none),
+  getOr: (key: string, fallback: T) => (key in _data ? _data[key] : fallback),
+  getAll: () => _data,
+  set: (key: string, value: T) => lookup({ ..._data, [key]: value }),
+  remove: (key: string) =>
+    // prettier-ignore
+    pipe(
+      [key, _data] as const,
+      ([key, _data]) => removeEntryFromRecord(key, _data),
+      lookup
+    ),
+})
