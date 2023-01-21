116f8f3194aa6f9ac5f8d565c7bc6a712925d7e0
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-01-21 22:14:21 +0100

    add generate range from to

--- src/range/range.spec.ts
+++ src/range/range.spec.ts
@@ -1,5 +1,12 @@
-import { range } from './range'
+import { range, rangeFromTo } from './range'
 
 test('range', () => {
   expect(range(10)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+})
+
+test('rangeFromTo', () => {
+  expect(rangeFromTo(4, 8)).toEqual([4, 5, 6, 7])
+  expect(rangeFromTo(9, 3)).toEqual([])
+  expect(rangeFromTo(8, 9)).toEqual([8])
+  expect(rangeFromTo(8, 8)).toEqual([])
 })
--- src/range/range.ts
+++ src/range/range.ts
@@ -1,1 +1,3 @@
 export const range = (n: number) => [...Array(n)].map((_, i) => i)
+
+export const rangeFromTo = (from: number, to: number) => (to >= from ? range(to - from).map(n => n + from) : [])
