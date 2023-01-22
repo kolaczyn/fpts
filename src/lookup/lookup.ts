6ff57f052b016297031ae6436e61e750fe5fd55e
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-01-22 12:52:00 +0100

    fix not needed _data field in lookup table

--- src/lookup/lookup.ts
+++ src/lookup/lookup.ts
@@ -9,7 +9,6 @@ export type Lookup<T> = {
   getOr: (key: string, fallback: T) => T
   remove: (key: string) => Lookup<T>
   getAll: () => Record<string, T>
-  _data: Record<string, T>
 }
 
 const removeEntryFromRecord = <T>(key: string, record: Record<string, T>) =>
@@ -23,16 +22,15 @@ const removeEntryFromRecord = <T>(key: string, record: Record<string, T>) =>
       {}
     )
 
-export const lookup = <T>(_data: Record<string, T>): Lookup<T> => ({
-  _data,
-  get: (key: string) => (key in _data ? some(_data[key]) : none),
-  getOr: (key: string, fallback: T) => (key in _data ? _data[key] : fallback),
-  getAll: () => _data,
-  set: (key: string, value: T) => lookup({ ..._data, [key]: value }),
+export const lookup = <T>(record: Record<string, T>): Lookup<T> => ({
+  get: (key: string) => (key in record ? some(record[key]) : none),
+  getOr: (key: string, fallback: T) => (key in record ? record[key] : fallback),
+  getAll: () => record,
+  set: (key: string, value: T) => lookup({ ...record, [key]: value }),
   remove: (key: string) =>
     // prettier-ignore
     pipe(
-      [key, _data] as const,
+      [key, record] as const,
       ([key, _data]) => removeEntryFromRecord(key, _data),
       lookup
     ),
--- src/range/range.ts
+++ src/range/range.ts
@@ -1,3 +1,5 @@
 export const range = (n: number) => [...Array(n)].map((_, i) => i)
 
-export const rangeFromTo = (from: number, to: number) => (to >= from ? range(to - from).map(n => n + from) : [])
+export const rangeFromTo = (from: number, to: number) =>
+  //
+  to >= from ? range(to - from).map(n => n + from) : []
