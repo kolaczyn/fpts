ecccf3cde681041bf8381fc295c1aaa089d71e6a
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-01-22 12:43:53 +0100

    add tests for zip

---
+++ src/zip/zip.spec.ts
@@ -0,0 +1,34 @@
+import { zip } from './zip'
+
+xdescribe('zip', () => {
+  test('same length', () => {
+    const arr1 = [1, 2, 3]
+    const arr2 = ['one', 'two', 'three']
+
+    expect(zip(arr1, arr2)).toMatchObject([
+      [1, 'one'],
+      [2, 'two'],
+      [3, 'three'],
+    ])
+  })
+
+  test('first shorted', () => {
+    const arr1 = [1, 2]
+    const arr2 = ['one', 'two', 'three']
+
+    expect(zip(arr1, arr2)).toMatchObject([
+      [1, 'one'],
+      [2, 'two'],
+    ])
+  })
+
+  test('second shorter', () => {
+    const arr1 = [1, 2, 3]
+    const arr2 = ['one', 'two']
+
+    expect(zip(arr1, arr2)).toMatchObject([
+      [1, 'one'],
+      [2, 'two'],
+    ])
+  })
+})
---
+++ src/zip/zip.ts
@@ -0,0 +1,1 @@
+export const zip = <A, B>(_arr1: A[], _arr2: B[]): [a: A, b: B][] => []
