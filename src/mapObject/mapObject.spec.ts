e4f3543b80559872fa65c0c60b80529502a9831f
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-01-22 12:16:10 +0100

    add mapObject

---
+++ src/mapObject/mapObject.spec.ts
@@ -0,0 +1,14 @@
+import { mapObject } from './mapObject'
+
+test('mapObject', () => {
+  const obj = {
+    one: 1,
+    two: 2,
+    three: 3,
+  }
+  expect(mapObject(obj, n => n ** 2)).toMatchObject({
+    one: 1,
+    two: 4,
+    three: 9,
+  })
+})
