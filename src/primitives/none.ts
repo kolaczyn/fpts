d19a8c89bf54c3868ecc6cb210573d133f53dbc7
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-01-25 19:20:05 +0100

    make tests fail

--- src/primitives/none.ts
+++ src/primitives/none.ts
@@ -1,7 +1,9 @@
 export type None = {
   none: 'none'
+  bind: () => 'bind'
 }
 
 export const none: None = {
   none: 'none',
+  bind: () => 'bind',
 }
--- src/primitives/some.ts
+++ src/primitives/some.ts
@@ -1,7 +1,9 @@
 export type Some<T> = {
   some: T
+  bind: () => 'bind'
 }
 
 export const some = <T>(payload: T): Some<T> => ({
   some: payload,
+  bind: () => 'bind',
 })
