817bf78e073f3260e4c763edaddf50f4788da679
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-01-25 18:59:58 +0100

    add util checking for 'maybe'

--- src/maybe/maybe.ts
+++ src/maybe/maybe.ts
@@ -3,9 +3,14 @@ import { Some, some } from '../primitives/some'
 
 export type Maybe<T> = Some<T> | None
 
+export const isMaybe = <T>(payload: unknown): payload is Maybe<T> =>
+  // @ts-expect-error: TS is dumb
+  'some' in payload || 'none' in payload
 export const isSome = <T>(maybe: Maybe<T>): maybe is Some<T> => 'some' in maybe
 export const isNone = <T>(maybe: Maybe<T>): maybe is None => 'none' in maybe
 export const toSome = <T>(payload: T): Maybe<T> => some(payload)
+
+export const normalizeMaybe = <T>(value: Maybe<T> | T): Maybe<T> => (isMaybe(value) ? value : some(value))
 
 export const maybeCatch = <T>(fn: () => T) => {
   try {
