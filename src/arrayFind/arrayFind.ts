888b99eaaa454beaf13bb8606c4b697f78981ee2
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-01-26 21:03:06 +0100

    add arrayFind

---
+++ src/arrayFind/arrayFind.ts
@@ -0,0 +1,10 @@
+import { some, none, Maybe } from '..'
+
+// copied from Array methods type definition
+// didn't know a better way :/
+type FindPredicate<T> = (value: T, index: number, obj: T[]) => unknown
+
+export const arrayFind = <T>(arr: T[], predicate: FindPredicate<T>): Maybe<T> => {
+  const element = arr.find(predicate)
+  return element === undefined ? none() : some(element)
+}
